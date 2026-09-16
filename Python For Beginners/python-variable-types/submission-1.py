c0e6e0e016e63f6f8779514b5b5f03ee63c251fd
Devon Myers <117554606+DevonMyers04@users.noreply.github.com>
2026-09-16 19:09:39 -0400

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,13 @@
+integer_type=12
+float_type=7.8
+bool_type= True
+string_type="do"
+list_type=[2,3,5]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
